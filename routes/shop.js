0aebbdae17be94dd456fb1f95fc90d0f08d5e063
Bohdan Neskorozhenyy <bohdanne@wix.com>
2025-03-17 13:06:53 +0200

    Додано функціонал додавання продуктів та оновлено відображення магазину

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,10 @@
-const path = require('path');
-
 const express = require('express');
 
-const rootDir = require('../util/path');
-
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-  res.render('shop');
+  res.render('shop', { prods: adminData.products, docTitle: 'Shop' });
 });
 
 module.exports = router;
