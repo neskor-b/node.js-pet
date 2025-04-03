9b79cb7ced19247f0bf6540f8bccbed8e006925a
Bohdan Neskorozhenyy <bohdanne@wix.com>
2025-04-03 19:55:15 +0300

    used ejs

--- app.js
+++ app.js
@@ -2,13 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs.engine({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: '.hbs'}));
-app.set('view engine', 'hbs');
-// app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
