ca7088d6fdfa14aba63e73e69b94ee42a679312f
Bohdan Neskorozhenyy <bohdanne@wix.com>
2025-05-15 09:24:31 +0300

    Added ID

--- models/product.js
+++ models/product.js
@@ -1,15 +1,18 @@
 const fs = require('fs');
+const { v4 } = require('uuid');
 const constants = require('./constants');
 
 
 module.exports = class Product {
     constructor({ title, imageUrl, description, price }) {
+        this.id = Math.random().toString();
         this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
         this.price = price;
     }
     save(){
+        this.id = v4();
         return saveProductsToFile(this);
     }
     static fetchAll(){
