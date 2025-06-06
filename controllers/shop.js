8d6d185c3f857187f10c947702da0fc238cf1d4d
Bohdan Neskorozhenyy <bohdanne@wix.com>
2025-06-06 14:45:46 +0300

    Refactor shop controller and add product detail view
    - Updated `getProducts`, `getProduct`, `getIndex`, `getCart`, `getCheckout`, and `getOrders` methods in `controllers/shop.js` for improved formatting and consistency.
    - Added a new product detail view in `views/shop/product-detail.ejs` to display product information and an "Add to Cart" form.
    - Introduced a new CSS class `.centered` in `public/css/main.css` for centering content.

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,50 +2,58 @@ const Product = require('../models/product');
 const path = require('../util/path');
 
 exports.getProducts = async (req, res, next) => {
-    const products = await Product.fetchAll();
-    res.render('shop/product-list', { 
-        prods: products, 
-        pageTitle: 'All products', 
-        path: '/products', 
-        activeShop: true, 
-        hasProducts: products.length > 0 
-    });
-}
+  const products = await Product.fetchAll();
+  res.render('shop/product-list', {
+    prods: products,
+    pageTitle: 'All products',
+    path: '/products',
+    activeShop: true,
+    hasProducts: products.length > 0,
+  });
+};
 
 exports.getProduct = async (req, res, next) => {
-    const prodId = req.params.productId;
-    const product = await Product.findById(prodId);
-    res.render('shop/product-detail', {
-        product: product,
-    });
-}
+  const prodId = req.params.productId;
+  const product = await Product.findById(prodId);
+  res.render('shop/product-detail', {
+    product: product,
+    pageTitle: product.title,
+    path: '/products',
+  });
+};
 
 exports.getIndex = async (req, res, next) => {
-    const products = await Product.fetchAll();
-    res.render('shop/index', { 
-        prods: products, 
-        pageTitle: 'Shop', 
-        path: '/', 
-    });
-}
+  const products = await Product.fetchAll();
+  res.render('shop/index', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+  });
+};
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        pageTitle: 'Your Cart', 
-        path: '/cart', 
-    });
-}
+  res.render('shop/cart', {
+    pageTitle: 'Your Cart',
+    path: '/cart',
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect('/cart');
+};
 
 exports.getCheckout = (req, res, next) => {
-    res.render('shop/checkout', {
-        pageTitle: 'Checkout',
-        path: '/checkout',
-    });
-}
+  res.render('shop/checkout', {
+    pageTitle: 'Checkout',
+    path: '/checkout',
+  });
+};
 
 exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', {
-        pageTitle: 'Orders',
-        path: '/orders',
-    });
-}
+  res.render('shop/orders', {
+    pageTitle: 'Orders',
+    path: '/orders',
+  });
+};
