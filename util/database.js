900d8cc9725f0777d93681de6f69c9082ab01b7f
Bohdan Neskorozhenyy <bohdanne@wix.com>
2025-12-20 17:36:11 +0200

    Add dotenv and mysql2 dependencies; create database connection pool

---
+++ util/database.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const mysql2 = require('mysql2');
+
+const pool = mysql2.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+});
+
+module.exports = pool.promise();
